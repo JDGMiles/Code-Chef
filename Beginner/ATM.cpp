1a5a6a9dddd75094a5a4286acf8cfda745762264
James Miles <jimdgmiles@gmail.com>
2016-04-18 17:39:25 +0100

    Removed "int"

--- Beginner/ATM.cpp
+++ Beginner/ATM.cpp
@@ -7,7 +7,7 @@ int main() {
   int centbalance = dollarbalance*100;
 
   if(centwithdrawal%500==0 && (centbalance-centwithdrawal-50)>=0){
-    cout << int(centbalance-centwithdrawal-50)/100 << "." << (centbalance-centwithdrawal-50)%100 << (centbalance-centwithdrawal-50)%10;
+    cout << (centbalance-centwithdrawal-50)/100 << "." << (centbalance-centwithdrawal-50)%100 << (centbalance-centwithdrawal-50)%10;
   }
   else{
     cout << (centbalance/100) << "." << centbalance%100 << centbalance%10;
